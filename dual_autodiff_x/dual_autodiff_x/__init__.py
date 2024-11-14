043add7f5eeb1a327e87f7c39c6eed210ddbac14
Laura Just Fung <18justfungl2@gmail.com>
2024-11-14 20:24:48 +0000

    attempt

--- dual_autodiff_x/dual_autodiff_x/__init__.py
+++ dual_autodiff_x/dual_autodiff_x/__init__.py
@@ -1,1 +1,5 @@
+# dual_autodiff_x/__init__.py
 from .dual import Dual
+import numpy as np
+
+__all__ = ['Dual']
--- dual_autodiff_x/setup.py
+++ dual_autodiff_x/setup.py
@@ -1,8 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import numpy as np
+import sys
+
+ext_modules = [
+    Extension(
+        name = "dual_autodiff_x",
+        sources = ["dual_autodiff_x/dual.pyx"],
+        include_dirs = [np.get_include()],
+    )
+]
 setup(
     name="dual_autodiff_x",
-    packages = find_packages(where="dual_autodiff_x"),
-    ext_modules=cythonize('dual_autodiff_x/dual.pyx')
+    version ="0.0.1",
+    description="A Python package for automatic differentiation using dual numbers.",
+    packages = ["dual_autodiff_x/dual_autodiff_x"],
+    ext_modules=cythonize(ext_modules),
+    python_requires=">=3.10"
     )
