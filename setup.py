a2f0e9b0dfff4efb8ddda1e3ea1cb9c8eab9e231
Laura Just Fung <18justfungl2@gmail.com>
2024-11-15 17:00:25 +0000

    pyproject.toml update

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'matplotlib',
-        'jupyter',
-        'pytest'
+        'numpy'
     ],
 )
