f444f472874573ab94c8f91d98cbcd1fcf88f6d3
lj441 <18justfungl2@gmail.com>
2024-11-17 19:19:15 +0000

    initial files

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# dual_autodiff/__init__.py
+from dual_autodiff.dual import Dual
+import numpy as np
+
+__all__ = ['Dual']
---
+++ dual_autodiff_x/__init__.py
@@ -0,0 +1,4 @@
+# dual_autodiff_x/__init__.py
+import numpy as np
+from dual_autodiff_x.dual import Dual
+__all__ = ['Dual']
